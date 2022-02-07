3ab30efbf66a7056e2d07213f6169e8d49c1d4b4
yasushi miyazaki <yasushi1002-s14@outlook.com>
2022-02-07 18:47:45 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/style.css">
+        <title>Portfolio</title>
+    </head>
+
+    <body>
+        <header id="header">
+            <h1 class="header-title">
+                <a href="index.php"><span>P</span>ortfolio</a>
+            </h1>
+        </header>
+
+        <main>
+            <div class="wrapper">
+                <h2><span>P</span>rofile</h2>
+                <img src="img/profile.png" alt="">
+                <p>テック 太郎</p>
+                <span>HTML / CSS / PHP / MySQL / Laravel / Adobe XD</span>
+                <p class="text">
+                    ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。
+                    ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。
+                    ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。
+                </p>
+            </div>
+            <div class="icon">
+                <a href="#"><img src="img/github.png" alt=""></a>
+                <a href="#"><img src="img/twitter.png" alt=""></a>
+            </div>
+            <div id="work" class="content">
+                <h2 class="site-title"><span>W</span>orks</h2>
+                <ul>
+                    <li><img src="img/work_1.png" alt="">
+                        <p>Twitterクローン</p>
+                        <p>HTML / CSS / PHP / MySQL</p>
+                        <a href="#">Webサイト</a>
+                        <a href="#">GitHub</a>
+                    </li>
+                    <li><img src="img/work_2.png" alt="">
+                        <p>カリキュラム管理システム</p>
+                        <p>CodeIgniter / MySQL</p>
+                        <a href="#">Webサイト</a>
+                        <a href="#">GitHub</a>
+                    </li>
+                    <li><img src="img/work_3.png" alt="">
+                        <p>ラーニングシステム</p>
+                        <p>Laravel / MySQL</p>
+                        <a href="#">Webサイト</a>
+                        <a href="#">GitHub</a>
+                    </li>
+                </ul>
+            </div>
+        </main>
+        <footer id="footer">
+            <p>Copyright &copy; TECH I.S., Inc.</p>
+        </footer>
+    </body>
+</html>
